d696effd40c94a52337b8ec0cacdbaeb89baf24d
Maria Paula Amaya <paulaamaya012@gmail.com>
2026-02-10 21:25:11 -0500

    [creación de codigo][codigo primera pagina]

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ const boton = document.getElementById("boton");
+const texto = document.getElementById("habilidad");
+
+boton.addEventListener("click", function () {
+    texto.textContent = "Manejo básico de análisis estadístico y visualización de datos.";
+});
